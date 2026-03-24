c89ee21f3ef2c212596be44d7048a87e2ec36a8f
Yanzhao Wang <yannzhaow@gmail.com>
2026-03-24 14:27:45 +0100

    testing ci

--- test/unit_tests/test_handler.cpp
+++ test/unit_tests/test_handler.cpp
@@ -2,3 +2,5 @@
 #include <gtest/gtest.h>
 
 TEST(centipede_handler, constructor) { auto handler = centipede::core::Handler{}; }
+
+TEST(centipede_handler, constructor_destructor) { auto handler = centipede::core::Handler{}; }
