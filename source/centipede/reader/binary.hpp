4f83fd406ac02fd52382076f5b5c006bae1b7e8b
Yanzhao Wang <yannzhaow@gmail.com>
2026-03-25 13:56:37 +0100

    add reader_api

--- source/centipede/reader/binary.hpp
+++ source/centipede/reader/binary.hpp
@@ -1,3 +1,51 @@
+#pragma once
+
+#include "centipede/data/entry.hpp"
+#include "centipede/util/return_types.hpp"
+#include <cstddef>
+#include <string>
+#include <vector>
+
 namespace centipede::reader
 {
-}
+    /**
+     * @class Class for reading binary files.
+     * @brief To be added ...
+     *
+     * To be added ...
+     */
+    class Binary
+    {
+      public:
+        /**
+         * @class Configuration for the class.
+         * @brief To be added ...
+         *
+         * To be added ...
+         */
+        struct Config
+        {
+            std::string in_filename = "output.bin"; //!< Output binary filename.
+        };
+
+        /**
+         * @brief Default constructor.
+         */
+        Binary() = default;
+
+        /**
+         * @brief Initialization of the instance.
+         *
+         * detailed description
+         * @return name description
+         */
+        [[nodiscard]] auto init() -> EnumError<>;
+
+        [[maybe_unused]] auto read_one_entry() -> EnumError<std::size_t>;
+
+        [[nodiscard]] auto get_current_entry() const -> const auto& { return entry_buffer_; }
+
+      private:
+        std::vector<EntryPoint<>> entry_buffer_; //!< A vector containing all entrypoints of the current entry.
+    };
+} // namespace centipede::reader
