578f598b89cc668de80807f08fb619c6dce86c42
Baryonics <benedikt.leicher@gmail.com>
2026-07-01 00:39:17 +0200

    Testing the progress indicator!

--- test/integration_tests/test_reader.cpp
+++ test/integration_tests/test_reader.cpp
@@ -1,9 +1,13 @@
 #include "centipede/reader/binary.hpp"
+#include "centipede/util/progress_indicator.hpp"
 #include <cstdlib>
 #include <print>
+#include <ranges>
 
 auto main() -> int
 {
+    static_assert(std::ranges::range<centipede::reader::Binary>);
+    static_assert(std::ranges::input_range<centipede::reader::Binary>);
     auto reader = centipede::reader::Binary{ centipede::reader::Binary::Config{ .in_filename = "output.bin" } };
     auto init_err = reader.init();
     if (not init_err.has_value())
@@ -12,7 +16,9 @@ auto main() -> int
         return EXIT_FAILURE;
     }
 
-    for ([[maybe_unused]] const auto& entry : reader)
+    auto progress_adaotor = centipede::progress::ProgressAdaptor{};
+    for ([[maybe_unused]] const auto& entry :
+         reader | progress_adaotor(reader.get_file_size(), [&reader]() { return reader.get_last_entry_bytes(); }))
     {
     }
 
