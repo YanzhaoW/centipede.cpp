178381504a88fc22146e9dd4e6ec5bbbe8d0d659
Baryonics <benedikt.leicher@gmail.com>
2026-07-01 12:32:53 +0200

    Added () overload to iterate up to a certain number of elements. Needs fixing

--- source/centipede/util/progress_indicator.hpp
+++ source/centipede/util/progress_indicator.hpp
@@ -14,6 +14,7 @@ namespace centipede::progress
     //  1) iterate over n elements (instead of increment_fun)
     //  2) iterate by 1 (for standard ranges)
     //  ProgressAdaptor constructor that takes some config struct (to config the prog bar)
+    //  Error Handling!
     class ProgressAdaptor;
 
     using IncrementFunT = std::function<std::size_t()>;
@@ -41,6 +42,20 @@ namespace centipede::progress
             return ProgressView<RangeT>{ this, std::forward<RangeT>(range), total_size_n, increment_fun };
         }
 
+        template <typename RangeT>
+            requires std::ranges::range<RangeT>
+        auto operator()(RangeT&& range, std::size_t total_size_n)
+        {
+            return ProgressView<RangeT>{
+                this, std::forward<RangeT>(range), total_size_n, []() -> std::size_t { return 1; }
+            };
+        }
+
+        auto operator()(std::size_t total_size_n)
+        {
+            return ProgressClosure{ {}, this, total_size_n, []() -> std::size_t { return 1; } };
+        }
+
         auto operator()(std::size_t total_size_n, IncrementFunT increment_fun)
         {
             return ProgressClosure{ {}, this, total_size_n, increment_fun };
@@ -51,6 +66,7 @@ namespace centipede::progress
         struct ProgressView
         {
             using IteratorType = std::ranges::iterator_t<RangeT>;
+            using SentinelType = std::ranges::sentinel_t<RangeT>;
 
             ProgressView(ProgressAdaptor* progress_adaptor,
                          RangeT&& base_range,
@@ -65,41 +81,53 @@ namespace centipede::progress
 
             auto begin()
             {
-                auto it = Iterator{ progress_adaptor_, this, base_range_.begin() };
-
-                it.add_progress();
-
-                return it;
+                return Iterator{
+                    progress_adaptor_, this, std::ranges::begin(base_range_), std::ranges::end(base_range_)
+                };
             }
 
-            auto end() { return base_range_.end(); }
+            auto end() { return Sentinel{}; }
+            struct Sentinel
+            {
+            };
 
             class Iterator
             {
               public:
-                Iterator(ProgressAdaptor* progress_adaptor, ProgressView* progress_view, IteratorType current_it)
+                Iterator(ProgressAdaptor* progress_adaptor,
+                         ProgressView* progress_view,
+                         IteratorType current_it,
+                         SentinelType end_it)
                     : progress_adaptor_(progress_adaptor)
                     , progress_view_(progress_view)
                     , current_it_(current_it)
+                    , end_it_(end_it)
                 {
                 }
 
                 auto operator++()
                 {
                     ++current_it_;
+                    ++element_count_;
                     add_progress();
                     return *this;
                 }
 
                 auto operator*() { return *current_it_; }
 
-                auto operator==(auto& other) { return current_it_ == other; }
+                bool operator==(Sentinel)
+                {
+                    return current_it_ == end_it_ || element_count_ >= progress_view_->total_size_n_;
+                }
 
-                auto operator!=(auto& other) { return current_it_ != other; }
+                bool operator!=(Sentinel s) { return !(*this == s); }
 
-                auto operator==(const auto& other) const { return current_it_ == other; }
+                bool operator==(Sentinel) const
+                {
+                    return current_it_ == end_it_ || element_count_ >= progress_view_->total_size_n_;
+                }
 
-                auto operator!=(const auto& other) const { return current_it_ != other; }
+                bool operator!=(Sentinel s) const { return !(*this == s); }
 
                 void add_progress()
                 {
@@ -120,6 +148,8 @@ namespace centipede::progress
                 ProgressAdaptor* progress_adaptor_;
                 ProgressView* progress_view_;
                 IteratorType current_it_;
+                SentinelType end_it_;
+                std::size_t element_count_{};
                 std::size_t count_n_{};
                 bool finished_{ false };
             };
