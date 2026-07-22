2dab3c8f0a557813000a6818ac54f43e0fd6bff2
Baryonics <benedikt.leicher@gmail.com>
2026-07-22 11:21:51 +0200

    Adjusted integration tests for ProgressAdaptor in reader::Binary

--- test/integration_tests/test_reader.cpp
+++ test/integration_tests/test_reader.cpp
@@ -3,8 +3,13 @@
 #include "centipede/util/progress_indicator.hpp"
 #include <array>
 #include <cstdlib>
+#include <indicators/color.hpp>
+#include <indicators/font_style.hpp>
+#include <indicators/progress_bar.hpp>
+#include <indicators/setting.hpp>
 #include <print>
 #include <ranges>
+#include <vector>
 
 auto main() -> int
 {
@@ -18,7 +23,20 @@ auto main() -> int
         return EXIT_FAILURE;
     }
 
-    auto progress_adaptor = centipede::progress::ProgressAdaptor{};
+    auto progress_adaptor = centipede::progress::ProgressAdaptor{
+        centipede::progress::config::BarWidth{ 50 },
+        centipede::progress::config::Start{ "[" },
+        centipede::progress::config::Fill{ "=" },
+        centipede::progress::config::Lead{ ">" },
+        centipede::progress::config::Remainder{ " " },
+        centipede::progress::config::End{ "]" },
+        centipede::progress::config::PostfixText{ "Reading binary data" },
+        centipede::progress::config::ForegroundColor{ centipede::progress::ProgressColor::green },
+        centipede::progress::config::ShowPercentage{ true },
+        centipede::progress::config::FontStyles{
+            std::vector<centipede::progress::ProgressFontStyle>{ centipede::progress::ProgressFontStyle::bold } }
+    };
+
     for ([[maybe_unused]] const auto& entry :
          reader | progress_adaptor(reader.get_file_size(), [&reader]() { return reader.get_last_entry_bytes(); }))
     {
@@ -39,10 +57,24 @@ auto main() -> int
         return EXIT_FAILURE;
     }
 
+    if (const auto progress_adaptor_status = progress_adaptor.get_status();
+        progress_adaptor_status != centipede::ErrorCode::success)
+    {
+        std::println(stderr, "Error: {}", progress_adaptor_status);
+        return EXIT_FAILURE;
+    }
+
     auto array = std::array{ 1, 2, 3, 4 };
 
     for ([[maybe_unused]] auto elem : array | progress_adaptor(array.size()))
     {
+    }
+
+    if (const auto progress_adaptor_status = progress_adaptor.get_status();
+        progress_adaptor_status != centipede::ErrorCode::success)
+    {
+        std::println(stderr, "Error: {}", progress_adaptor_status);
+        return EXIT_FAILURE;
     }
 
     return EXIT_SUCCESS;
